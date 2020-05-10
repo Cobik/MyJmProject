7f776c660f2bb67b918e8aecc59c59c0620df438
javidanhajizada <hacizade55@gmail.com>
2020-05-11 02:00:47 +0400

    Last Task(1.1.5)

---
+++ myjmproject/src/main/java/servlets/DeleteServlet.java
@@ -0,0 +1,31 @@
+package servlets;
+
+
+import service.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/admin/delete")
+public class DeleteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        try {
+            if (UserServiceImpl.getInstance().deleteUser(Long.parseLong(id))){
+                resp.sendRedirect("/admin/main");
+                resp.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                resp.getWriter().println("Have a problem");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
